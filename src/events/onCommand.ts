170a732c42ab1b5f6ffff113c14644c13be5435f
Andrey Shishkov <Anderial94@gmail.com>
2020-05-05 10:00:16 +0300

    fix style webkit-scrollbar in popup window

--- src/events/onCommand.ts
+++ src/events/onCommand.ts
@@ -1,11 +1,26 @@
 import { MegaMegaMessage, ITabInfo, IWindowInfo } from './../model'
-import { EvetnHandlerWorcer,  } from './../core'
+import { EvetnHandlerWorcer } from './../core'
 
 //  >>>>>>>>>   Defining onCommand event
 chrome.commands.onCommand.addListener(function (command: string) {
-    var testData = {
-        event: "onCommand",
-        command_: command
-    };
-    MegaMegaMessage(testData);
+    chrome.tabs.getSelected((tab: chrome.tabs.Tab) => {
+        var testData = {
+            event: "onCommand",
+            command_: command,
+            current_tab_id: tab.id,
+            current_window_id: tab.windowId
+        };
+
+        MegaMegaMessage(testData);
+    });
 });
+
+/**
+ *  onCommand event handler
+ * @param {WindowInfo} targetWindowInfo Information about browser window and tabs
+ * @param {number} targetTabId Id target tab
+ */
+function onCommandFunction(targetWindowInfo: IWindowInfo, targetTabId: number): IWindowInfo {
+
+    return undefined;
+}
